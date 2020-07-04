581aa9e0cb1dcfb0fb1bf092a0e8c6a3f0d4011b
wangcheng <453451180@qq.com>
2020-07-04 12:18:18 +0800

    init

---
+++ nacos-config/src/test/java/com/github/cdtft/microservices/nacosconfig/NacosConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cdtft.microservices.nacosconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
