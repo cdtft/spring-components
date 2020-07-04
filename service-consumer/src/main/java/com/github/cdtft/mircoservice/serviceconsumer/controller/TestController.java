8f1829923a5d772d96fa91ee68cb15c3d5e6f8ca
wangcheng <453451180@qq.com>
2020-07-04 16:09:33 +0800

    ok

--- service-consumer/src/main/java/com/github/cdtft/mircoservice/serviceconsumer/controller/TestController.java
+++ service-consumer/src/main/java/com/github/cdtft/mircoservice/serviceconsumer/controller/TestController.java
@@ -24,6 +24,6 @@ public class TestController {
 
     @GetMapping(value = "/echo/{str}")
     public String echo(@PathVariable String str) {
-        return restTemplate.getForObject("http://service-provider/echo/" + str, String.class);
+        return restTemplate.getForObject("http://provider/echo/" + str, String.class);
     }
 }
