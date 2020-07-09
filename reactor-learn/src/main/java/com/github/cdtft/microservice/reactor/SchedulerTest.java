9243b16e92a6dac6077381337e66b34296d395d0
wangcheng <453451180@qq.com>
2020-07-09 10:49:21 +0800

    scheduler

--- reactor-learn/src/main/java/com/github/cdtft/microservice/reactor/SchedulerTest.java
+++ reactor-learn/src/main/java/com/github/cdtft/microservice/reactor/SchedulerTest.java
@@ -1,10 +1,25 @@
 package com.github.cdtft.microservice.reactor;
 
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
 /**
  * @author : wangcheng
  * @date : 2020年07月09日 10:25
  */
 public class SchedulerTest {
 
+    public static void publishOnTest() {
+        Scheduler s = Schedulers.newParallel("parallel-scheduler", 4);
+        Flux<String> flux = Flux.range(1, 2)
+                .map(i -> i + 10)
+                .publishOn(s)
+                .map(i -> "value " + i);
+        new Thread(() -> flux.subscribe(System.out::println)).start();
+    }
 
+    public static void main(String[] args) {
+        publishOnTest();
+    }
 }
